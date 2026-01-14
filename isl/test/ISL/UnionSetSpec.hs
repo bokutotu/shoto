b6d0d53c98d56e26bc9d985a491479270ea6b060
bokutotu <mushin.hudoushin@gmail.com>
2026-01-15 02:22:19 +0900

    tmp

--- isl/test/ISL/UnionSetSpec.hs
+++ isl/test/ISL/UnionSetSpec.hs
@@ -1,22 +1,21 @@
-module ISL.UnionSetSpec where
+module ISL.UnionSetSpec (spec) where
 
---
--- import           ISL
--- import           Test.Hspec
---
--- spec :: Spec
--- spec = do
---     describe "ISL Union Set" $ do
---         it "can parse and convert union set to string" $ do
---             result <- runISL $ do
---                 uset <- unionSet "{ A[i] : 0 <= i < 10; B[j] : 0 <= j < 5 }"
---                 unionSetToString uset
---             result `shouldBe` Right "{ A[i] : 0 <= i <= 9; B[j] : 0 <= j <= 4 }"
---
---         it "can compute union set union" $ do
---             result <- runISL $ do
---                 u1 <- unionSet "{ A[i] : 0 <= i < 5 }"
---                 u2 <- unionSet "{ B[j] : 0 <= j < 3 }"
---                 combined <- unionSetUnion u1 u2
---                 unionSetToString combined
---             result `shouldBe` Right "{ A[i] : 0 <= i <= 4; B[j] : 0 <= j <= 2 }"
+import           ISL
+import           Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "ISL Union Set" $ do
+        it "can parse and convert union set to string" $ do
+            result <- runISL $ do
+                uset <- unionSet "{ A[i] : 0 <= i < 10; B[j] : 0 <= j < 5 }"
+                unionSetToString uset
+            result `shouldBe` Right "{ A[i] : 0 <= i <= 9; B[j] : 0 <= j <= 4 }"
+
+        it "can compute union set union" $ do
+            result <- runISL $ do
+                u1 <- unionSet "{ A[i] : 0 <= i < 5 }"
+                u2 <- unionSet "{ B[j] : 0 <= j < 3 }"
+                combined <- unionSetUnion u1 u2
+                unionSetToString combined
+            result `shouldBe` Right "{ A[i] : 0 <= i <= 4; B[j] : 0 <= j <= 2 }"
